eb063ae6ee8dd1429da0d98ab11e64919a756f63
eselman <erkalselman@gmail.com>
2018-11-17 23:29:43 +0100

    -

--- src/UndoList/WithSave.elm
+++
@@ -1,8 +0,0 @@
-module UndoList.WithSave exposing (a)
-
-{-| This is an undolist which saves the index of the last saved state
--}
-
-
-a =
-    42
---
+++ src/UndoListWithSave.elm
@@ -0,0 +1,9 @@
+module UndoListWithSave exposing (a)
+
+{-| This is an undolist which stores the last saved state.
+The terminology is adapted from [elm-community/undo-redo](https://package.elm-lang.org/packages/elm-community/undo-redo/latest/)
+-}
+
+
+a =
+    42
