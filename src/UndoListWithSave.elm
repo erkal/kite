3c5f2e6f63d8ca2281b14cbea7ab200fc06c0be7
eselman <erkalselman@gmail.com>
2018-11-18 11:43:44 +0100

    worked on UndoListWithSave

--- src/UndoListWithSave.elm
+++ src/UndoListWithSave.elm
@@ -1,13 +1,14 @@
-module UndoListWithSave exposing (UndoListWithSave)
+module UndoListWithSave exposing
+    ( UndoListWithSave
+    , fresh
+    , undo, redo, new, mapPresent
+    , savePresent, resetToSaved
+    , presentIsTheLastSaved
+    , hasPast, hasFuture
+    , goTo
+    )
 
-{-| ( UndoList
-, undo, redo, fresh, new, mapPresent, forget, reset
-, hasPast, hasFuture
-, toList, fromList
-, goTo
-)
-
-UndoList Data Structure keeping also track of the last saved state.
+{-| UndoList Data Structure keeping also track of the last saved state.
 It allows the user to check fast whether the current state has changed after last save.
 
 The terminology of functions is adapted to [elm-community/undo-redo](https://package.elm-lang.org/packages/elm-community/undo-redo/latest/)
@@ -18,19 +19,34 @@ The terminology of functions is adapted to [elm-community/undo-redo](https://pac
 @docs UndoListWithSave
 
 
+# Constructor
+
+@docs fresh
+
+
 # Basic Operations
 
-@docs undo, redo, fresh, new, mapPresent, forget, reset
+@docs undo, redo, new, mapPresent
 
 
-# Query UndoList
+# Saving Related Operations
+
+@docs savePresent, resetToSaved
+
+
+# Saving Related Queries
+
+@docs presentIsTheLastSaved
+
+
+# Queries
 
 @docs hasPast, hasFuture
 
 
-# Conversions
+# Expensive Operations
 
-@docs toList, fromList
+@docs goTo
 
 -}
 
@@ -41,77 +57,159 @@ import UndoList as UL exposing (UndoList)
 -}
 type UndoListWithSave state
     = UndoListWithSave
-        { savedAt : Maybe Int
+        { save : Save
         , savedState : state
-        , ul : UndoList state
+        , uL : UndoList state
+        }
+
+
+{-| Note that the last saved state can lie in the past, present or in the future, or it can be lost. The latter happens because the future gets removed by `UndoList.new`.
+-}
+type Save
+    = Lost
+    | At Int
+
+
+
+---------------
+-- INTERNALS --
+---------------
+
+
+goToHelper : Int -> UndoList a -> UndoList a
+goToHelper i uL =
+    let
+        l =
+            uL |> UL.toList
+
+        past =
+            l |> List.take i |> List.reverse
+    in
+    case l |> List.drop i of
+        present :: future ->
+            UndoList past present future
+
+        _ ->
+            uL
+
+
+mapUL : (UndoList a -> UndoList a) -> UndoListWithSave a -> UndoListWithSave a
+mapUL up (UndoListWithSave { save, savedState, uL }) =
+    UndoListWithSave
+        { save = save
+        , savedState = savedState
+        , uL = up uL
         }
 
 
 
--------------------------------
--- Basic UndoList Operations --
--------------------------------
---undo : UndoList state -> UndoList state
---undo { past, present, future } =
---    case past of
---        [] ->
---            UndoList past present future
---        x :: xs ->
---            UndoList xs x (present :: future)
---redo : UndoList state -> UndoList state
---redo { past, present, future } =
---    case future of
---        [] ->
---            UndoList past present future
---        x :: xs ->
---            UndoList (present :: past) x xs
---goTo : Int -> UndoList state -> UndoList state
---goTo i undoList =
---    let
---        l =
---            undoList |> toList
---        past =
---            l |> List.take i |> List.reverse
---    in
---    case l |> List.drop i of
---        present :: future ->
---            UndoList past present future
---        _ ->
---            undoList
---fresh : state -> UndoList state
---fresh state =
---    UndoList [] state []
---new : state -> UndoList state -> UndoList state
---new event { past, present } =
---    UndoList (present :: past) event []
---forget : UndoList state -> UndoList state
---forget { present, future } =
---    UndoList [] present future
---reset : UndoList state -> UndoList state
---reset { past, present } =
---    case past of
---        [] ->
---            fresh present
---        x :: xs ->
---            reset (UndoList xs x [])
+-----------------
+-- Constructor --
+-----------------
+
+
+fresh : a -> UndoListWithSave a
+fresh state =
+    UndoListWithSave
+        { save = At 0
+        , savedState = state
+        , uL = UL.fresh state
+        }
+
+
+
 ----------------------
--- UndoList Queries --
+-- Basic Operations --
 ----------------------
---hasPast : UndoList state -> Bool
---hasPast =
---    not << List.isEmpty << .past
---hasFuture : UndoList state -> Bool
---hasFuture =
---    not << List.isEmpty << .future
----------------------------
--- Functional Operations --
----------------------------
---mapPresent : (a -> a) -> UndoList a -> UndoList a
---mapPresent f { past, present, future } =
---    UndoList past (f present) future
---toList : UndoList state -> List state
---toList { past, present, future } =
---    List.reverse past ++ [ present ] ++ future
---fromList : state -> List state -> UndoList state
---fromList present future =
---    UndoList [] present future
+
+
+undo : UndoListWithSave a -> UndoListWithSave a
+undo =
+    mapUL UL.undo
+
+
+redo : UndoListWithSave a -> UndoListWithSave a
+redo =
+    mapUL UL.undo
+
+
+new : a -> UndoListWithSave a -> UndoListWithSave a
+new event =
+    mapUL (UL.new event)
+
+
+mapPresent : (a -> a) -> UndoListWithSave a -> UndoListWithSave a
+mapPresent up =
+    mapUL (UL.mapPresent up)
+
+
+
+-------------------------------
+-- Saving Related Operations --
+-------------------------------
+
+
+savePresent : UndoListWithSave a -> UndoListWithSave a
+savePresent (UndoListWithSave { save, savedState, uL }) =
+    UndoListWithSave
+        { save = At (UL.lengthPast uL)
+        , savedState = uL.present
+        , uL = uL
+        }
+
+
+resetToSaved : UndoListWithSave a -> UndoListWithSave a
+resetToSaved (UndoListWithSave { savedState }) =
+    fresh savedState
+
+
+
+----------------------------
+-- Saving Related Queries --
+----------------------------
+
+
+{-| Fast check whether the state has changed after the last save.
+This can be used in order to mark in the GUI, whether a file has changed after the last save.
+-}
+presentIsTheLastSaved : UndoListWithSave a -> Bool
+presentIsTheLastSaved (UndoListWithSave { save, uL }) =
+    case save of
+        At i ->
+            i == UL.lengthPast uL
+
+        Lost ->
+            False
+
+
+
+--------------
+--  Queries --
+--------------
+
+
+{-| To determine whether the undo button should be disabled.
+-}
+hasPast : UndoListWithSave a -> Bool
+hasPast (UndoListWithSave { uL }) =
+    UL.hasPast uL
+
+
+{-| To determine whether the redo button should be disabled.
+-}
+hasFuture : UndoListWithSave a -> Bool
+hasFuture (UndoListWithSave { uL }) =
+    UL.hasFuture uL
+
+
+
+--------------------------
+-- Expensive Operations --
+--------------------------
+
+
+{-| This is expensive but necessary if the whole history is visualized and the user wants to jump to states by clicking on history items.
+-}
+goTo : Int -> UndoListWithSave a -> UndoListWithSave a
+goTo i =
+    mapUL (goToHelper i)
